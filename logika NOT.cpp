c7b9519eb9db49cb34472949ac3eab1dddb7aa39
Wildan Firdaus Damanik <45458756+satu33@users.noreply.github.com>
2019-01-09 16:46:10 +0700

    Add files via upload

---
+++ logika NOT.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+main()
+{
+    int w1,w2,x1,x2,Output;
+    w1=1;w2=1
+    cout<<"inputan x1=";
+    cin>>x1;
+    cout<<"inputan x2=";
+    cin<<x2;
+    Output=(x1*w1)+(x2*w2);
+    if(Output>=2)
+        cout<<"Output(Y)NOT=1"<<endl;
+    else
+        cout<<"Output(Y)NOT=0"<<endl;
+}
